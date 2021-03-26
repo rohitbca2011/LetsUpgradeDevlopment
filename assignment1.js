14efcef62a307469ea0d07b5bdb1c8538994ee76
rohitbca2011 <69254804+rohitbca2011@users.noreply.github.com>
2021-03-26 16:51:14 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,11 @@
+var firstEle=document.getElementsByTagName('h1');
+console.log(firstEle[0].innerHTML);
+
+var secondEle=document.getElementsByClassName('organisation');
+console.log(secondEle[0].innerHTML);
+
+var thirdEle=document.getElementById('name');
+console.log(thirdEle);
+
+
+firstEle[0].innerHTML="HELLO JAVASCRIPT"
